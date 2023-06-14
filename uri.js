0a82017cced2d8e6240dd9e31bdac56476462256
Grukur <darold.trench@gmail.com>
2023-06-14 16:46:44 -0400

    uploading

--- uri.js
+++ uri.js
@@ -2,8 +2,8 @@ import pg from 'pg';
 
 const { Pool, Client } = pg;
 
-const connectionString = 
-'postgresql://node_user:node@localhost:5432/practica_db'
+const connectionString =
+    'postgresql://node_user:node@localhost:5432/practica_db'
 
 
 const pool = new Pool({
